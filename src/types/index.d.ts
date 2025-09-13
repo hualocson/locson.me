7910cbbdaa1ffe5bf1ecde6fe32c72dcb7fd9781
locson <hualocson@gmail.com>
2025-09-13 21:14:49 +0700

    Add Frontmatter interface to TypeScript definitions for enhanced metadata handling

---
+++ src/types/index.d.ts
@@ -0,0 +1,15 @@
+interface Frontmatter {
+  title?: string;
+  display?: string;
+  date?: string;
+  duration?: string;
+  place?: string;
+  placeLink?: string;
+  subtitle?: string;
+  draft?: boolean;
+  lang?: string;
+  wrapperClass?: string;
+  class?: string;
+  tocAlwaysOn?: boolean;
+  art?: "plum" | "dots" | "random";
+}
