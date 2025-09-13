115dbd4b74f21bc0e856939bcde909ed0873e4e1
locson <hualocson@gmail.com>
2025-09-13 21:15:41 +0700

    Add pre-processing functionality for MDX with magic link support and predefined link mappings

---
+++ src/lib/pre-process-mdx.ts
@@ -0,0 +1,15 @@
+import processMagicLink from "./mdx-plugins/magic-link";
+
+const preProcessMDX = (source: string) => {
+  return processMagicLink(source, {
+    linksMap: {
+      "Next.js": "https://nextjs.org",
+      "Tailwind CSS": "https://tailwindcss.com/",
+      "Shadcn UI": "https://ui.shadcn.com",
+      React: "https://react.dev",
+      TypeScript: "https://www.typescriptlang.org",
+    },
+  });
+};
+
+export default preProcessMDX;
