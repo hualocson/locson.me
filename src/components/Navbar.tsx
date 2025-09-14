525a0f95ffb1834706ad15890c75e35d66a77fdc
locson <hualocson@gmail.com>
2025-09-14 21:14:05 +0700

    Update Navbar component to include new icons for navigation links, enhance layout with flex properties, and improve accessibility with aria-labels and titles for external links.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react";
 
 import Link from "next/link";
 
-import { Mail } from "lucide-react";
+import { Github, LightbulbIcon, Mail, NotebookPenIcon } from "lucide-react";
 
 import ToggleThemeButton from "./ToggleThemeButton";
 
@@ -18,36 +18,50 @@ export default function Navbar() {
   return (
     <nav className="grid w-full grid-cols-[auto_max-content] p-8">
       <div className="m-auto" />
-      <div className="grid grid-flow-col place-items-center gap-5">
+      <div className="grid grid-flow-col place-items-center gap-4">
         {/* Navigation Links */}
         <Link
           href="/posts"
-          className="text-muted-foreground hover:text-foreground font-medium transition-colors duration-200"
+          className="text-muted-foreground hover:text-foreground flex items-center justify-center font-medium transition-colors duration-200"
         >
-          Blog
+          <span className="hidden md:inline-block">Blog</span>
+          <span className="inline-block md:hidden">
+            <NotebookPenIcon className="size-4" />
+          </span>
         </Link>
         <Link
           href="/projects"
-          className="text-muted-foreground hover:text-foreground font-medium transition-colors duration-200"
+          className="text-muted-foreground hover:text-foreground flex items-center justify-center font-medium transition-colors duration-200"
         >
-          Projects
+          <span className="hidden md:inline-block">Projects</span>
+          <span className="inline-block md:hidden">
+            <LightbulbIcon className="size-4" />
+          </span>
         </Link>
 
-        {/* Mail and theme toggle */}
         {/* Mail Icon */}
-        <a
+        <Link
+          href="https://github.com/hualocson"
+          className="text-muted-foreground hover:text-foreground p-1 transition-colors duration-200"
+          aria-label="Github"
+          title="Github"
+        >
+          <Github className="size-4" />
+        </Link>
+        <Link
           href="mailto:hualocson@gmail.com"
           className="text-muted-foreground hover:text-foreground p-1 transition-colors duration-200"
           aria-label="Email"
+          title="Email"
         >
-          <Mail className="size-5" />
-        </a>
+          <Mail className="size-4" />
+        </Link>
 
         {/* Theme Toggle */}
         {mounted ? (
           <ToggleThemeButton />
         ) : (
-          <span className="bg-muted/60 inline-flex size-5 rounded" />
+          <span className="bg-muted/60 inline-flex size-4 rounded" />
         )}
       </div>
     </nav>
