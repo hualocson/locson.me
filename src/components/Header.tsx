3a237587e67172880247307137050bf8eb3d71de
locson <hualocson@gmail.com>
2025-09-14 21:14:51 +0700

    Add scroll-to-top button in Header component, utilizing useWindowScroll hook for scroll position management and improved user experience.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,11 +2,28 @@
 
 import React from "react";
 
+import useWindowScroll from "@/app/hooks/useWindownScroll";
+import { cn } from "@/lib/utils";
+import { ArrowUpIcon } from "lucide-react";
+
 import Navbar from "./Navbar";
 
 const Header = () => {
+  const { scrollPosition, scrollTo } = useWindowScroll();
   return (
     <header className="z-40">
+      <button
+        title="Scroll to top"
+        onClick={() => scrollTo(0, 0)}
+        className={cn(
+          "fixed right-3 bottom-3 z-50 flex size-10 cursor-pointer items-center justify-center rounded-full transition-all duration-300 hover:bg-[#8883] hover:opacity-100 active:bg-[#8882] active:opacity-100 print:hidden",
+          scrollPosition.y > 300
+            ? "opacity-30"
+            : "pointer-events-none opacity-0"
+        )}
+      >
+        <ArrowUpIcon className="size-5" />
+      </button>
       <Navbar />
     </header>
   );
