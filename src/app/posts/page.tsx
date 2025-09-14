efb8973afa04f4a37e9e8d8bcd983f5a3c4d7a5a
locson <hualocson@gmail.com>
2025-09-14 21:15:44 +0700

    Enhance PostsPage component to include ListPosts for displaying post items and add metadata for improved SEO and social sharing, including Open Graph and Twitter card configurations.

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,10 +1,43 @@
-import { compileMDX } from "@/lib/mdx";
+import { Metadata } from "next";
 
+import getBaseUrl from "@/lib/get-base-url";
+import { compileMDX, getPostsFrontmatter } from "@/lib/mdx";
+
+import ListPosts from "@/components/ListPosts";
 import PostWrapper from "@/components/PostWrapper";
 
 async function PostsPage() {
   const { code, frontmatter } = await compileMDX("/posts/index");
-  return <PostWrapper frontmatter={frontmatter} code={code} />;
+  const items = getPostsFrontmatter();
+  return (
+    <PostWrapper frontmatter={frontmatter} code={code}>
+      <ListPosts items={items} />
+    </PostWrapper>
+  );
 }
 
 export default PostsPage;
+const baseUrl = getBaseUrl();
+const title = "Blog - Loc Son";
+const ogImage = `${baseUrl}/api/og?title=${encodeURIComponent(title)}`;
+
+export const metadata: Metadata = {
+  title,
+  openGraph: {
+    title,
+    description: "",
+    type: "website",
+    url: `${baseUrl}/posts`,
+    images: [
+      {
+        url: ogImage,
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title,
+    description: "",
+    images: [ogImage],
+  },
+};
