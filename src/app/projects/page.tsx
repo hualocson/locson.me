f67aaa422997848d34788ad347c134af6c936d6d
locson <hualocson@gmail.com>
2025-09-14 21:15:52 +0700

    Add metadata to ProjectsPage for improved SEO and social sharing, including Open Graph and Twitter card configurations.

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,3 +1,6 @@
+import { Metadata } from "next";
+
+import getBaseUrl from "@/lib/get-base-url";
 import { compileMDX } from "@/lib/mdx";
 
 import PostWrapper from "@/components/PostWrapper";
@@ -8,3 +11,29 @@ async function ProjectsPage() {
 }
 
 export default ProjectsPage;
+
+const baseUrl = getBaseUrl();
+const title = "Projects - Loc Son";
+const description = "List of projects that I am proud of";
+const ogImage = `${baseUrl}/api/og?title=${encodeURIComponent(title)}`;
+
+export const metadata: Metadata = {
+  title,
+  openGraph: {
+    title,
+    description,
+    type: "website",
+    url: `${baseUrl}/projects`,
+    images: [
+      {
+        url: ogImage,
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title,
+    description,
+    images: [ogImage],
+  },
+};
