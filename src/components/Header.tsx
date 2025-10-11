51b260a73a099bdf587490a2e9a112c4aefc76cf
locson <hualocson@gmail.com>
2025-10-11 16:41:53 +0700

    Update Header component: modify header positioning and enhance button active state transition for improved user experience.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -12,7 +12,7 @@ import NewLogo from "./NewLogo";
 const Header = () => {
   const { scrollPosition, scrollTo } = useWindowScroll();
   return (
-    <header className="z-40">
+    <header className="fixed inset-x-0 top-0 z-40">
       <Link
         className="absolute m-5 flex h-12 w-12 items-center justify-center p-2 outline-none select-none xl:fixed"
         href={"/"}
@@ -23,7 +23,7 @@ const Header = () => {
         title="Scroll to top"
         onClick={() => scrollTo(0, 0)}
         className={cn(
-          "fixed right-3 bottom-3 z-50 flex size-10 cursor-pointer items-center justify-center rounded-full transition-all duration-300 hover:bg-[#8883] hover:opacity-100 active:bg-[#8882] active:opacity-100 print:hidden",
+          "fixed right-3 bottom-3 z-50 flex size-10 cursor-pointer items-center justify-center rounded-full transition-all duration-150 hover:bg-[#8883] hover:opacity-100 active:scale-[0.97] active:bg-[#8882] active:opacity-100 print:hidden",
           scrollPosition.y > 300
             ? "opacity-30"
             : "pointer-events-none opacity-0"
