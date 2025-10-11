761a5239ad7a6e9f939371b6a284e498bccde162
locson <hualocson@gmail.com>
2025-10-11 16:41:03 +0700

    Enhance ToggleThemeButton: implement view transition effects and refactor theme toggling logic for improved user experience and performance.

--- src/components/ToggleThemeButton.tsx
+++ src/components/ToggleThemeButton.tsx
@@ -1,22 +1,65 @@
 "use client";
 
+import { useRef } from "react";
+
 import { cn } from "@/lib/utils";
 import { Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
+import { flushSync } from "react-dom";
 
 function ToggleThemeButton() {
-  const { setTheme, theme } = useTheme();
+  const ref = useRef<HTMLButtonElement>(null);
+
+  const { theme, setTheme } = useTheme();
+
+  const toggleDarkMode = async (isDarkMode: boolean) => {
+    /**
+     * Return early if View Transition API is not supported
+     * or user prefers reduced motion
+     */
+    if (
+      !ref.current ||
+      !document.startViewTransition ||
+      window.matchMedia("(prefers-reduced-motion: reduce)").matches
+    ) {
+      setTheme(isDarkMode ? "dark" : "light");
+      return;
+    }
+
+    await document.startViewTransition(() => {
+      flushSync(() => {
+        setTheme(isDarkMode ? "dark" : "light");
+      });
+    }).ready;
+
+    const { top, left, width, height } = ref.current.getBoundingClientRect();
+    const x = left + width / 2;
+    const y = top + height / 2;
+    const right = window.innerWidth - left;
+    const bottom = window.innerHeight - top;
+    const maxRadius = Math.hypot(Math.max(left, right), Math.max(top, bottom));
+
+    document.documentElement.animate(
+      {
+        clipPath: [
+          `circle(0px at ${x}px ${y}px)`,
+          `circle(${maxRadius}px at ${x}px ${y}px)`,
+        ],
+      },
+      {
+        duration: 500,
+        easing: "ease-in-out",
+        pseudoElement: "::view-transition-new(root)",
+      }
+    );
+  };
 
   return (
     <button
-      style={
-        {
-          "--border-width": "1px",
-        } as React.CSSProperties
-      }
-      onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+      ref={ref}
+      onClick={() => toggleDarkMode(theme === "dark" ? false : true)}
       className={cn(
-        "text-muted-foreground hover:text-foreground glass-card relative cursor-pointer rounded-md p-2 transition-all duration-200 ease-in-out active:scale-[0.97] active:brightness-110"
+        "text-muted-foreground hover:text-foreground relative cursor-pointer rounded-md p-2 transition-all duration-200 ease-in-out active:scale-[0.97] active:brightness-110"
       )}
       aria-label="Toggle theme"
       title="Toggle Color Schema"
