5528218a84ee022c2a8bc935f665f5f224de94ad
locson <hualocson@gmail.com>
2025-10-19 16:26:44 +0700

    Refactor img component in mdx-components.tsx: wrap img in a div for improved styling and add a decorative overlay with a ring effect for enhanced visual presentation.

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -21,13 +21,16 @@ const components = {
     return <Link href={href} {...rest} />;
   },
   img: ({ className, ...props }) => (
-    <img
-      className={cn(
-        className,
-        "mx-0 my-[2.6em] w-full scale-105 rounded-lg shadow"
-      )}
-      {...props}
-    />
+    <div className="relative rounded-xl">
+      <img
+        className={cn(
+          className,
+          "mx-0 my-[2.6em] w-full scale-105 rounded-[inherit] shadow"
+        )}
+        {...props}
+      />
+      <div className="pointer-events-none absolute inset-0 scale-105 rounded-[inherit] ring-1 ring-black/10 ring-inset dark:ring-white/10" />
+    </div>
   ),
 } satisfies MDXComponents;
 
